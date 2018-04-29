c2791e527406696e9ef06f12b346be98bd078370
pizzadeesnutz <Arcadio@iMac.local>
2018-04-28 23:09:01 -0400

    Another Commit
    Mongoose is now helping message posts save to back end and
    automatically be called up when clicking post…

--- MEANStack2018/back-end/server.js
+++ MEANStack2018/back-end/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Message = mongoose.model('Message', {
+  msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -12,8 +17,22 @@ app.use(function(req, res, next){
 
 app.post('/api/message', function(req,res){
   console.log(req.body);
+  var message = new Message(req.body);
+  message.save();
   res.status(200);
 })
+
+mongoose.connect("mongodb://localhost:27017/test", function(err, db){
+  if(!err){
+    GetMessages();
+  }
+})
+
+function GetMessages(){
+  Message.find({}).exec(function(err,result){
+    console.log(result);
+  });
+}
 
 var server = app.listen(5000, function(){
   console.log('listening on port ', server.address().port);
