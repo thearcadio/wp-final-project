7c2ffa569d35a23af7bceff7b7a0d13233a70d3a
pizzadeesnutz <Arcadio@iMac.local>
2018-04-28 23:29:55 -0400

    FINALLY DONE WITH VIDEOS
    The videos are all done and it seems like everything is working fine…
    need to quit everything and try to make sure I can still run it all.

--- MEANStack2018/front-end/src/app/main/main.controller.js
+++ MEANStack2018/front-end/src/app/main/main.controller.js
@@ -3,6 +3,14 @@ export class MainController {
     'ngInject';
     this.$http = $http;
     this.postMessage = this.postMessage.bind(this);
+    this.getMessages();
+  }
+
+  getMessages(){
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/message').then(function(result){
+      vm.messages = result.data;
+    });
   }
 
   postMessage(){
