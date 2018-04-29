00f2d2f4dbe3a4fd078ef6c183a59b2d4ed15dc5
pizzadeesnutz <Arcadio@iMac.local>
2018-04-28 23:16:48 -0400

    Almost Done...
    Committing at the end of another video, I can now post to the back end
    and view the data on a separate tab

--- MEANStack2018/back-end/server.js
+++ MEANStack2018/back-end/server.js
@@ -15,6 +15,8 @@ app.use(function(req, res, next){
   next();
 })
 
+app.get('/api/message', GetMessages)
+
 app.post('/api/message', function(req,res){
   console.log(req.body);
   var message = new Message(req.body);
@@ -24,13 +26,13 @@ app.post('/api/message', function(req,res){
 
 mongoose.connect("mongodb://localhost:27017/test", function(err, db){
   if(!err){
-    GetMessages();
+    //
   }
 })
 
-function GetMessages(){
+function GetMessages(req, res){
   Message.find({}).exec(function(err,result){
-    console.log(result);
+    res.send(result);
   });
 }
 
